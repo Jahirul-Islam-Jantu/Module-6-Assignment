0806202a485d4c041f89a5100f102a6cae582ff0
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-01-07 09:22:45 +0600

    modified

---
+++ Assets/js/main.js
@@ -0,0 +1,13 @@
+const input = document.getElementById("age");
+const submit = document.getElementById("submit");
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
+
+submit.addEventListener
+
+
+
+
+
+
